fcfdd10e6345badff1c1ef028dd97820566b9a07
Irina Korovnikova <irina.shako19@gmail.com>
2024-11-18 22:56:29 +0500

    add exercises

---
+++ sem7/exercise1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая выведет все натуральные числа в
+// промежутке от M до N. Используйте рекурсию и не используйте циклы.
+// Пример:
+// ● Вход: M = 1, N = 5
+// ● Выход: 1, 2, 3, 4, 5
+// ● Вход: M = 4, N = 8
+// ● Выход: 4, 5, 6, 7, 8
+
+void PrintNaturalNumbersToMax(int num1, int num2)
+{
+    if (num1 > num2) return;
+    Console.Write(num1);
+    if (num1 < num2) Console.Write(", ");
+    PrintNaturalNumbersToMax(num1 + 1, num2);
+}
+
+void PrintNaturalNumbersToMin(int num1, int num2)
+{
+    if (num1 < num2) return;
+    Console.Write(num1);
+    if (num1 > num2) Console.Write(", ");
+    PrintNaturalNumbersToMin(num1 - 1, num2);
+}
+
+Console.WriteLine("Введите число M: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2)
+{
+    Console.WriteLine("Ошибка! Введите два разных числа");
+}
+
+PrintNaturalNumbersToMax(number1, number2);
+PrintNaturalNumbersToMin(number1, number2);
---
+++ sem7/exercise3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте произвольный массив. Выведите его элементы, начиная с конца.
+// Используйте рекурсию и не используйте циклы.
+// Пример:
+// ● Вход: {1, 2, 3, 4, 5}
+// ● Выход: 5, 4, 3, 2, 1
+// ● Вход: {10, 20, 30, 40}
+// ● Выход: 40, 30, 20, 10
+
+void PrintArrayReverse(int[] array, int index)
+{
+if (index < 0) return; 
+Console.Write(array[index]);
+if (index > 0) Console.Write(", ");
+PrintArrayReverse(array, index - 1);
+}
+
+int[] arr = { 5, 7, 10, 4, 1 };
+Console.WriteLine("Массив в обратном порядке:");
+PrintArrayReverse(arr, arr.Length - 1);
+
+
